f8d7a1993a6177c96636832b877c1556410ca45f
Qinyi Cai <CaiQinyi@foxmail.com>
2018-04-19 17:55:50 +0800

    Add files via upload

---
+++ C_11_8.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_11_8 : MonoBehaviour
+{
+    public AudioSource gunshot;
+    public void OnGUI()
+    {
+        //当按下开火键时播放开枪音效
+        if(GUILayout.Button("File"))
+        {
+            gunshot.Play();
+        }
+    }
+}
