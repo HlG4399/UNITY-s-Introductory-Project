e6069e00d26a212a118ef0050ead3134940f4b8c
Qinyi Cai <CaiQinyi@foxmail.com>
2018-04-28 23:08:02 +0800

    Add files via upload

---
+++ C_13_3_3_3.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class C_13_3_3_3 : MonoBehaviour
+{
+    public List<string> assertBundleNames;
+    private int maxCount = 4;
+    private string address = "http://192.168.1.104/Unity/ABs/";
+    private Dictionary<string, WWW> wwwDownloading = new Dictionary<string, WWW>();
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var item in wwwDownloading)
+        {
+            if(item.Value.isDone)
+            {
+                item.Value.assetBundle.Unload(true);
+                item.Value.Dispose();
+                wwwDownloading.Remove(item.Key);
+            }
+        }
+
+        foreach(string s in assertBundleNames)
+        {
+            if(wwwDownloading.Count<maxCount)
+            {
+                Load(s);
+                assertBundleNames.Remove(s);
+            }
+        }
+    }
+
+    void Load(string abName)
+    {
+        WWW www = WWW.LoadFromCacheOrDownload(abName, 0);
+        wwwDownloading.Add(abName, www);
+    }
+}
