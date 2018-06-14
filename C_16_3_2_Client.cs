811bd39471cd6c4146f82a65765ef4c1c4c392a1
Qinyi Cai <CaiQinyi@foxmail.com>
2018-06-14 20:19:25 +0800

    Add files via upload

---
+++ C_16_3_2_Client.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_16_3_2_Client : MonoBehaviour
+{
+    void Start()
+    {
+        //请求CarzyBalls游戏
+        MasterServer.RequestHostList("CrazyBalls");
+    }
+
+    void OnGUI()
+    {
+        //得到所有服务器
+        HostData[] data = MasterServer.PollHostList();
+        //显示服务器列表，并可加入
+        foreach(HostData hostData in data)
+        {
+            GUILayout.BeginHorizontal();
+            string text = hostData.gameName + "房间人数：" + hostData.connectedPlayers + "/" + hostData.playerLimit;
+            GUILayout.Label(text);
+            string hostInfo = "";
+            foreach(string hostIP in hostData.ip)
+            {
+                hostInfo += hostIP + ":";
+            }
+            hostInfo += hostData.port;
+            if(GUILayout.Button("加入"))
+            {
+                Network.Connect(hostData);
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
