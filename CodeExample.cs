e773915f427b0ccd0fed839c8ff2bcba8262be80
Qinyi Cai <CaiQinyi@foxmail.com>
2018-06-23 22:48:06 +0800

    Add files via upload

---
+++ CodeExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CodeExample : MonoBehaviour
+{
+    public List<string> playerNames;
+
+    void OnGUI()
+    {
+#if Test
+        if(GUILayout.Button("Print player names"))
+        {
+            for (int i = 0; i < playerNames.Count; i++)
+            {
+                Debug.Log("player:" + playerNames[i]);
+            }
+        }
+#endif
+    }
+}
